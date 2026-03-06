c1f929fb96941da79f3805e56008ae0eff4d667a
meitsojj <30435225+meitsojj@users.noreply.github.com>
2026-03-06 23:55:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
